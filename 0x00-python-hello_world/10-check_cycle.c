0c11b91cb2d4f12f764340b9f2a5039bd61931a9
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-10-22 19:30:54 +0300

    TASK 10 c circle completed

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle in it.
+ * @list: head of the list
+ * Return: 0 OR 1
+*/
+int check_cycle(listint_t *list)
+{
+	listint_t *tail;
+
+	if (!list)
+		return (0);
+	tail = list->next;
+	while (tail != list)
+	{
+		if (!tail)
+			return (0);
+		tail = tail->next;
+	}
+	return (1);
+}
