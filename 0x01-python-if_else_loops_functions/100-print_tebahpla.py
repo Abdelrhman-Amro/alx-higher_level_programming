a386112e999bf4d9c4c1eb181dedce3a58be8cd3
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-10-23 15:59:54 +0300

    TASKs (9, 10, 11, 12, 100, 101)

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+
+for i in range(90, 64, -1):
+    x = i
+    if not i % 2:
+        x = i + 32
+    print("{:c}".format(x), end="")
--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+
+
+def remove_char_at(str, n):
+    news = str[:n] + str[n+1:]
+    return news
