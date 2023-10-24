6deabbf019ff57d611c3dc2280339e2d2e691542
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-10-24 18:15:18 +0300

    task 2 completed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,11 @@
 
 if __name__ == "__main__":
     from sys import argv
+
     narg = len(argv)
     if narg < 2:
         print("0 arguments.")
     else:
-        print(argv)
+        print("{} arguments:".format(narg-1))
+        for i in range(1, narg):
+            print("{}: {}".format(i, argv[i]))
