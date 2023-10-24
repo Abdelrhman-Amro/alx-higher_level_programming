51f81273f31b750de3c8f0564c3c5c825c7d7b61
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-10-24 19:45:17 +0300

    task 3 done

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    sum = 0
+    ln = len(argv)
+    for i in range(1, ln):
+        sum += int(argv[i])
+    print(sum)
