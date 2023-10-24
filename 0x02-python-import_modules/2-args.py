ad76d289366ecdd8411d9ab27467429a838a4f8f
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-10-24 19:12:22 +0300

    task 2 done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,8 @@ if __name__ == "__main__":
     narg = len(argv)
     if narg < 2:
         print("0 arguments.")
+    elif narg == 2:
+        print("1 argument:\n1: {}".format(argv[1]))
     else:
         print("{} arguments:".format(narg-1))
         for i in range(1, narg):
