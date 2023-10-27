8a06be5a8981e503b0cf763e0bd8a1fbe8d7b406
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-10-27 09:58:03 +0200

    check another solution

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,6 @@ def print_matrix_integer(matrix=[[]]):
         return None
     for row in matrix:
         ln = len(row)
-        if not ln:
-            print()
         for idx in range(ln):
-            print("{:d}".format(row[idx]), end="\n" if idx == ln - 1 else " ")
+            print("{:d}".format(row[idx]), end="" if idx == ln - 1 else " ")
+        print()
