21f5531c05cf61c1f2e0ccbef16b6653681dcd66
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-10-27 14:36:19 +0200

    Task 10 completed

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    return list(map(lambda x: True if x % 2 == 0 else False, my_list))
+    new_list = [False if x % 2 else True for x in my_list]
+    return new_list
