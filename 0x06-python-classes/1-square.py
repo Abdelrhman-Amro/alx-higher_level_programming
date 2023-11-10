89c3fe8b48214d9fa671471a73410aa9c9583f6f
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-11-10 14:37:56 +0200

    Ok now task 1 completed

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Square Area"""
+
+
+class Square:
+    """Class Square to deal with squares"""
+
+    def __init__(self, size):
+        """Assign square size to private instance attribute"""
+        self.__size = size
+
+    pass
