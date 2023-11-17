8fad0ea7816a4426b59ca1a289711c0b16c02729
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-11-17 16:06:54 +0200

    task 1 completed

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Define MyList class"""
+
+
+class MyList(list):
+    """
+        class inherit from list
+
+        Methods:
+            print_sorted(): print sorted list
+    """
+
+    def print_sorted(self):
+        """print list in ascending order"""
+        Sorted_list = sorted(self)
+        print(Sorted_list)
