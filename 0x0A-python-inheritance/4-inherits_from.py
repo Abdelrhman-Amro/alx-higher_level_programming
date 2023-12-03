0118b5130c8765b23c88a841ca6a69fca04b3c8e
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-12-04 01:17:47 +0200

    task 4 completed

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -15,7 +15,6 @@ def inherits_from(obj, a_class):
         bolean: True if the check is true
         bolean: Fals otherwise
     """
-    if issubclass(type(obj), a_class) and type(obj != a_class):
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
-    else:
-        return False
+    return False
