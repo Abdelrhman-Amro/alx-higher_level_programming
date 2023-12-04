ba2eb3f47dc7cfed26b86417a2bfd97ac7f33c17
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-12-04 06:19:45 +0200

    task 10 completed

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""Define Square class that inherit from BasesGeometry"""
+BG = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(BG):
+    """Square Class"""
+
+    def __init__(self, size):
+        """
+            Constructor
+
+            Args:
+                size (int): Side length of the square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """Calculate the area of the square"""
+        return self.__size * self.__size
