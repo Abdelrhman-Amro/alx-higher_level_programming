50d1942af998dda37fdebd2cd1e3adf398dba065
Abdelrhman Amro <abdamro288dev@gmail.com>
2023-12-04 04:51:52 +0200

    task 8 completed

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""Define Rectangle class that inherit from BasesGeometry"""
+BG = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BG):
+    """Rectangle Class"""
+
+    def __init__(self, width, height):
+        """
+            Constructor
+
+            Args:
+                Width (int): width of the rectangle
+                height (int): height of the rectangle
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
