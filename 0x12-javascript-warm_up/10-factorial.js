26d2b96fc2c3ec51eb2bb26850c630464ed70189
Abdelrhman Amro <abdamro288dev@gmail.com>
2024-02-13 19:10:52 +0200

    Task 10 completed

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+function facto (x) {
+  if (x === 0) {
+    return (1);
+  }
+  return (x * facto(x - 1));
+}
+
+const n = Number(process.argv[2]);
+if (isNaN(n)) {
+  console.log(1);
+} else {
+  console.log(facto(n));
+}
