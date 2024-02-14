d6f4706ac1492e24c17e60eae792e8c7bc09529e
Abdelrhman Amro <abdamro288dev@gmail.com>
2024-02-14 10:10:31 +0200

    done

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 if (process.argv.length <= 3) {
-  console.log(1);
+  console.log(0);
 } else {
   const arrNumbers = process.argv.slice(2).sort(function (a, b) { return a - b; });
   console.log(arrNumbers[1]);
