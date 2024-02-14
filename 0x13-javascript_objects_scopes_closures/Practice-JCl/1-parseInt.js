9c2a625e68aa60d1eded73c0237f9278cb976a5a
Abdelrhman Amro <abdamro288dev@gmail.com>
2024-02-14 18:34:22 +0200

    done

---
+++ 0x13-javascript_objects_scopes_closures/Practice-JCl/1-parseInt.js
@@ -0,0 +1,8 @@
+let num = '10'
+console.log(num + '--->' + typeof(num))
+num = parseInt(num)
+console.log(num + '--->' + typeof(num))
+num = 5.5
+console.log(num + '--->' + typeof(num))
+num = parseInt(num)
+console.log(num + '--->' + typeof(num))
---
+++ 0x13-javascript_objects_scopes_closures/Practice-JCl/2-Equality.js
@@ -0,0 +1,15 @@
+// There is === & == sympols
+// === type and valu
+// == just value
+
+console.log(5 === 5); // Output: true
+console.log(5 === "5"); // Output: false
+console.log(true === 1); // Output: false
+
+console.log(5 == 5); // Output: true
+console.log(5 == "5"); // Output: true
+console.log(true == 1); // Output: true
+
+console.log(0 == ''); // Output: true
+console.log(0 === ''); // Output: false
+console.log(false == ''); // Output: true
