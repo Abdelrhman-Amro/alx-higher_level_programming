b0daa1f3b576a3386c025e49e9ccb64300b394a9
Abdelrhman Amro <abdamro288dev@gmail.com>
2024-03-21 23:33:07 +0200

    task 11 completed

--- 0x0D-SQL_introduction/commands.sh
+++ 0x0D-SQL_introduction/commands.sh
@@ -4,6 +4,15 @@ cat 8-count_89.sql | mysql -hlocalhost -uroot -p hbtn_0c_0 | tail -1
 ## 10 ##
 cat 10-top_score.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
 ## 11 ##
+cat 11-best_score.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
 ## 12 ##
+
 ## 13 ##
+
 ## 14 ##
+## 15 ##
+## 16 ##
+## 100 ##
+## 101 ##
+## 102 ##
+## 103 ##
