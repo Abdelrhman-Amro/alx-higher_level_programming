3719375a3abe21b0abe40ed78bf26c996223c6e3
Abdelrhman Amro <abdamro288dev@gmail.com>
2024-03-22 00:24:24 +0200

    firx task 15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- lists the number of records with the same score
-SELECT score, count(name) FROM second_table
+SELECT score, COUNT(name) AS number FROM second_table
 GROUP BY score
 ORDER BY number DESC;
