d98ccc7a8e9a948721a54b4b8fbe43ab5ec92b0a
Abdelrhman Amro <abdamro288dev@gmail.com>
2024-03-21 23:30:01 +0200

    task 10 sorting

--- 0x0D-SQL_introduction/commands.sh
+++ 0x0D-SQL_introduction/commands.sh
@@ -1,2 +1,9 @@
-##8##
+## 8 ##
 cat 8-count_89.sql | mysql -hlocalhost -uroot -p hbtn_0c_0 | tail -1
+## 9 ##
+## 10 ##
+cat 10-top_score.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+## 11 ##
+## 12 ##
+## 13 ##
+## 14 ##
