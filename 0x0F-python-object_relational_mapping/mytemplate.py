d4893adea5db69d2588580946bc743c9c0810933
Abdelrhman Amro <abdamro288dev@gmail.com>
2024-03-30 22:03:44 +0200

    task 6 completed

--- 0x0F-python-object_relational_mapping/mytemplate.py
+++ 0x0F-python-object_relational_mapping/mytemplate.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+""" MySQLdb """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3],
+        port=3306
+    )
+    cur = db.cursor()
+
+    query = ""
+    cur.execute(query)
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
