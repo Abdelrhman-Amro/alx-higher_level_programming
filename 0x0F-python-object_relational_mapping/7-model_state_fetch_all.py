98d320dcd26b1c4295df5f062cc9b20043690516
Abdelrhman Amro <abdamro288dev@gmail.com>
2024-03-31 02:37:07 +0200

    fix format of task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,5 +12,5 @@ if __name__ == "__main__":
     session = Session()
     q = session.query(State).order_by(State.id)
     for row in q:
-        print(row.id, ":", row.name)
+        print("{}: {}".format(row.id, row.name))
     session.close()
