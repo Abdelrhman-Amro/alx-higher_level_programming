cb1eb7af59d8617f121fd7ca7c904f26c255cbce
Abdelrhman Amro <abdamro288dev@gmail.com>
2024-03-31 02:56:04 +0200

    task 10 completed

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" SQLAlchemy """
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine("mysql://{}:{}@localhost:3360/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(engine)
+    session = Session()
+    q = session.query(State).filter(State.name.like(f"{sys.argv[4]}")).first()
+    if q is None:
+        print("Not found")
+    else:
+        print("{}".format(q.id))
+    session.close()
