86dd5e2f3629894aaa9582e98c72ba974f11c228
Abdelrhman Amro <abdamro288dev@gmail.com>
2024-04-08 07:19:54 +0200

    task 7 completed

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+Sends a request to the URL and displays the body of the response.
+"""
+if __name__ == "__main__":
+    from requests import get as GET
+    import requests
+    from sys import argv
+
+    url = argv[1]
+    rsp = GET(url)
+    if rsp.status_code >= 400:
+        print("Error code: {}".format(rsp.status_code))
+    else:
+        print(rsp.text)
