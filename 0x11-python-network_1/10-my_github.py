d614749ac264e66a3ce23f48731ec263b8e73bea
Abdelrhman Amro <abdamro288dev@gmail.com>
2024-04-08 08:14:48 +0200

    f

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+The letter must be sent in the variable q
+"""
+if __name__ == "__main__":
+    from requests import post as POST
+    from sys import argv
+
+    username = argv[1]
+    password = argv[2]
--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
-"""
-sends a POST request to http://0.0.0.0:5000/search_user
+"""A script tha:
+- takes in a letter
+- sends POST request to http://0.0.0.0:5000/search_user
 with the letter as a parameter.
-The letter must be sent in the variable q
 """
+import sys
+import requests
+
+
 if __name__ == "__main__":
-    from requests import post as POST
-    from sys import argv
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
 
-    if len(argv) == 1:
-        value = ""
-    else:
-        value = argv[1]
-
-    url = "http://0.0.0.0:5000/search_user"
-    payload = {'q': value}
-    rsp = POST(url, data=payload)
-
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
-        j = rsp.json()
-        if j is {}:
+        response = r.json()
+        if response == {}:
             print("No result")
         else:
-            print(f"[{j.get('id')}] {j.get('name')}")
+            print("[{}] {}".format(response.get("id"), response.get("name")))
     except ValueError:
         print("Not a valid JSON")
