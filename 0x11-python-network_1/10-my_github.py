449dca68b73d51f2a066480ff477aaef91c5472d
Abdelrhman Amro <abdamro288dev@gmail.com>
2024-04-08 08:26:08 +0200

    task 10 done

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,8 +5,12 @@ with the letter as a parameter.
 The letter must be sent in the variable q
 """
 if __name__ == "__main__":
-    from requests import post as POST
+    from requests import get as GET
     from sys import argv
 
     username = argv[1]
     password = argv[2]
+    url = "https://api.github.com/user"
+    up = (username, password)
+    r = GET(url, auth=up)
+    print(r.json().get('id'))
