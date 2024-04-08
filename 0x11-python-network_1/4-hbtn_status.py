0124a278ad012f0f6b54f377b100cd5d952ff1f4
Abdelrhman Amro <abdamro288dev@gmail.com>
2024-04-08 06:17:26 +0200

    task 4 completed

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""fetches https://alx-intranet.hbtn.io/status"""
+
+if __name__ == "__main__":
+    from requests import get as GET
+
+    url = "https://alx-intranet.hbtn.io/status"
+    rsp = GET(url)
+    print("Body response:")
+    print(f"\t- type: {type(rsp.text)}")
+    print(f"\t- content: {rsp.text}")
