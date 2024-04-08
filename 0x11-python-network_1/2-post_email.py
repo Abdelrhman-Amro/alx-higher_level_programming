121f9e4b17adaadeeea481be881afbdeeafe24b1
Abdelrhman Amro <abdamro288dev@gmail.com>
2024-04-08 03:38:01 +0200

    task 2 completed

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+Sends a POST request to the passed URL with the email as a parameter
+and displays the body of the response (decoded in utf-8)
+"""
+
+
+if __name__ == "__main__":
+    from urllib import request as req, parse as p
+    from sys import argv
+
+    url = argv[1]
+    email = argv[2]
+
+    data = {"email": email}
+    encoded_data = p.urlencode(data).encode('utf-8')
+    Req = req.Request(url, encoded_data, method='POST')
+
+    with req.urlopen(Req) as rsp:
+        print(rsp.read().decode('UTF-8'))
