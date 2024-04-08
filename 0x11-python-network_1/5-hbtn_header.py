0e3884c481d9cd9cfcd39af7c90dcecc92d3e230
Abdelrhman Amro <abdamro288dev@gmail.com>
2024-04-08 06:22:06 +0200

    task 5 completed

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+sends a request to the URL and displays
+the value of the variable X-Request-Idin the response header
+"""
+if __name__ == "__main__":
+    from requests import get as GET
+    from sys import argv
+
+    url = argv[1]
+    rsp = GET(url)
+    print(rsp.headers['X-Request-Id'])
