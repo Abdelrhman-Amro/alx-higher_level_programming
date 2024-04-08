a9bd48c5d5cbc519f97c8acc90e7fda6b8034e8a
Abdelrhman Amro <abdamro288dev@gmail.com>
2024-04-08 07:06:06 +0200

    post 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+sends a POST request to the passed URL with the email as a parameter
+and finally displays the body of the response.
+"""
+if __name__ == "__main__":
+    from requests import post as POST
+    from sys import argv
+
+    url = argv[1]
+    email = argv[2]
+    
+    params = {'email': email}
+    rsp = POST(url, data=params)
+    print(rsp.text)
