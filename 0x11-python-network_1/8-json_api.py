90b98a1f5132d04fba3791664e0bbb8f25fe0230
Abdelrhman Amro <abdamro288dev@gmail.com>
2024-04-08 07:57:26 +0200

    fix url

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     else:
         value = ""
 
-    url = "http://0.0.0.0:5000/search_use"
+    url = "http://0.0.0.0:5000/search_user"
     params = {'q': value}
     rsp = POST(url, data=params)
 
@@ -23,5 +23,5 @@ if __name__ == "__main__":
             print('No result')
         else:
             print(f"[{j.get('id')}] {j.get('name')}")
-    except:
-        print("Not a valid JSON")   
+    except ValueError:
+        print("Not a valid JSON")
