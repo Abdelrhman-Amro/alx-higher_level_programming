bf67d2c69ff38d98ffc2a35cc121b14869604efb
Abdelrhman Amro <abdamro288dev@gmail.com>
2024-07-03 18:21:26 +0300

    Complete task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+const argsNumber = process.argv.length - 2;
+if (argsNumber === 1) console.log("Argument found");
+else if (argsNumber > 1) console.log("Arguments found");
+else console.log("No argument");
