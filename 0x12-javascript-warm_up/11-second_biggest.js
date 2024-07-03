feadec09a2bd053d1d567f34c6cf0fb97689a43a
Abdelrhman Amro <abdamro288dev@gmail.com>
2024-07-03 12:35:15 +0300

    task 0 completed

--- 0x12-javascript-warm_up/11-second_biggest.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-if (process.argv.length <= 3) {
-  console.log(0);
-} else {
-  const arrNumbers = process.argv.slice(2).sort(function (a, b) { return a - b; });
-  console.log(arrNumbers[arrNumbers.length - 2]);
-}
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-const firstArg = Number(process.argv[2]);
-if (isNaN(firstArg)) {
-  console.log('Not a number');
-} else {
-  const n = Math.floor(firstArg);
-  console.log('My number: ' + n);
-}
